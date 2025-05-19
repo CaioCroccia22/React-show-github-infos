2c6bff56553c7fd3a4990871ef2051ea7c0d1d84
caiocroccia22 <caiocroccia22@gmail.com>
2025-05-19 20:25:17 -0300

    feat: implement 'sobre mim' section

--- src/containers/Sobre/Sobre.tsx
+++ src/containers/Sobre/Sobre.tsx
@@ -1,5 +1,6 @@
 import Paragraph from '../../components/Paragraph/Paragraph'
 import Title from '../../components/Title/Title'
+import { GithubSecao } from './styles'
 
 const Sobre = () => {
   return (
@@ -10,6 +11,16 @@ const Sobre = () => {
         quaerat aliquid similique excepturi quas debitis nihil harum sunt dicta
         vitae, odit culpa asperiores. Iure voluptate ab vitae dolor animi?
       </Paragraph>
+      <GithubSecao>
+        <img
+          src="https://github-readme-stats.vercel.app/api?username=caiocroccia22&show_icons=true&theme=merko"
+          alt="GitHub Stats"
+        />
+        <img
+          src="https://github-readme-stats.vercel.app/api/top-langs/?username=caiocroccia22&layout=compact&langs_count=8&theme=merko"
+          alt="Top Languages"
+        />
+      </GithubSecao>
     </section>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -7,6 +7,15 @@ const EstiloGlobal = createGlobalStyle`
     box-sizing: border-box;
     font-family: 'Inter', sans serif
   }
+
+
+  body{
+    padding-top: 80px;
+
+    @media (max-width: 768px){
+      padding-top: 16px;
+    }
+  }
 `
 
 export const Container = styled.div`
@@ -16,6 +25,12 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 128px auto;
   column-gap: 56px;
+  margin-top: 60px;
+
+  @media (max-width: 768px) {
+    max-width: 80%;
+    display: block;
+  }
 
   img {
     max-width: 100%;
